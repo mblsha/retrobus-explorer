5489480157c15741863fc9e2de29fd7ab4bdeee6
Michael Pishchagin <mblsha+gh@gmail.com>
2026-03-15 18:52:55 +1100

    Use PC-E500 signal names in raw Saleae tap

--- gateware/reference/spade-projects/sharp-pc-e500-card-spade/test/test_sharp_pc_e500_card.py
+++ gateware/reference/spade-projects/sharp-pc-e500-card-spade/test/test_sharp_pc_e500_card.py
@@ -11,7 +11,14 @@ async def _init(dut):
 
     dut.rst_n.value = 0
     dut.usb_rx.value = 1
-    dut.ffc_data.value = 0
+    dut.rw.value = 0
+    dut.addr.value = 0
+    dut.vcc2.value = 0
+    dut.data.value = 0
+    dut.ce1.value = 0
+    dut.ce6.value = 1
+    dut.nc.value = 0
+    dut.oe.value = 0
 
     await tick(dut.clk, 2)
     dut.rst_n.value = 1
@@ -19,32 +26,41 @@ async def _init(dut):
 
 
 @cocotb.test()
-async def low_ffc_data_pins_map_directly_to_saleae(dut):
+async def named_pc_e500_pins_map_directly_to_saleae(dut):
     await _init(dut)
 
-    dut.ffc_data.value = 0x00
+    dut.rw.value = 0
+    dut.addr.value = 0x00
     await tick(dut.clk, 1)
     assert int(dut.saleae.value) == 0x00
 
-    dut.ffc_data.value = 0x01
+    dut.rw.value = 1
+    dut.addr.value = 0x01
     await tick(dut.clk, 1)
-    assert int(dut.saleae.value) == 0x01
+    assert int(dut.saleae.value) == 0x0C
 
-    dut.ffc_data.value = 0xA5
+    dut.addr.value = 0x0F
     await tick(dut.clk, 1)
-    assert int(dut.saleae.value) == 0xA5
+    assert int(dut.saleae.value) == 0xDC
 
-    dut.ffc_data.value = 0x1FF
+    dut.addr.value = 0x0A
     await tick(dut.clk, 1)
-    assert int(dut.saleae.value) == 0xFF
+    assert int(dut.saleae.value) == 0x94
 
 
 @cocotb.test()
 async def unrelated_outputs_are_tied_off(dut):
     await _init(dut)
 
-    dut.ffc_data.value = (1 << 48) - 1
     dut.usb_rx.value = 0
+    dut.vcc2.value = 1
+    dut.data.value = 0xFF
+    dut.ce1.value = 1
+    dut.ce6.value = 0
+    dut.nc.value = 1
+    dut.oe.value = 1
+    dut.rw.value = 1
+    dut.addr.value = (1 << 18) - 1
     await tick(dut.clk, 1)
 
     assert int(dut.usb_tx.value) == 1
