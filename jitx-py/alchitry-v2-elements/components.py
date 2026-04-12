bd9e82addb44b05fb386b6f0b4781fecf9f7d418
Michael Pishchagin <mblsha+gh@gmail.com>
2026-04-12 23:46:06 +1000

    Add Alchitry V2 elements import project

---
+++ jitx-py/alchitry-v2-elements/components.py
@@ -0,0 +1,11 @@
+from src.components import (
+    AlchitryV2BothElement,
+    AlchitryV2BottomElement,
+    AlchitryV2TopElement,
+)
+
+__all__ = [
+    "AlchitryV2TopElement",
+    "AlchitryV2BottomElement",
+    "AlchitryV2BothElement",
+]
